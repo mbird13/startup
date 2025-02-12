b942c26b494f65977fad5120be2d8ac5290bff13
Mark Bird <markrbird13@gmail.com>
2025-02-11 19:05:21 -0700

    move all html to react

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import './home.css';
 
 export function Home() {    
     return (
-        <div className="body">
-            <h1>Home</h1>
-            <p>Welcome to the home page.</p>
+        <main>
+        <div>
+            <form method="get" action="results.html" class="input-form">
+                <h2>What's your next meal?</h2>
+                <label><input type="radio" name="meal" value="Breakfast" />Breakfast</label>
+                <label><input type="radio" name="meal" value="Lunch" />Lunch</label>
+                <label><input type="radio" name="meal" value="Dinner" />Dinner</label>    
+                <br />
+                <h2>How much time do you have?</h2>
+                <label><input type="number" />minutes</label>
+                <br />
+                <button type="submit">Search</button>
+
+            </form>
         </div>
+
+        <div class="notifications-bar">
+            <h2>Notifications</h2>
+            <ul>
+                <li>Claire is eating Miso Ramen today!</li>
+                <li>James favorited Drunken Noodles</li>
+                <li>Jimmy is eating Parmesan Chicken today!</li>
+            </ul>
+        </div>
+        </main>
+
     );
 }
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export function Login() {
     return (
         <main>
-            <div class="input-form">
+            <div className="input-form">
             <h2>Login</h2>
             <form method="get" action="home">
-                <input type="text" placeholder="Email" />
+                <input type="text" placeholder="email" />
                 <input type="password" placeholder="password" />
                 <button type="submit">Submit</button>
             </form>
