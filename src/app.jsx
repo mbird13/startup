213e17f5b99b52b328c44d59d6883e4ddda5a8af
Mark Bird <markrbird13@gmail.com>
2025-02-11 18:42:21 -0700

    add header and footer to react

--- src/app.jsx
+++ src/app.jsx
@@ -2,5 +2,25 @@ import React from 'react';
 import './main.css';
 
 export default function App() {
-  return <div className="body">App will display here</div>;
+  return <div className="body">
+    <header>
+            <nav>
+                <menu class="navigation">
+                    <li><a href="index.html">Login</a></li>
+                    <li><a href="home.html">Home</a></li>
+                    <li><a href="favorites.html">Favorites</a></li>
+                </menu>
+            </nav>
+            <hr />
+        </header>
+        <div>
+            Home
+        </div>
+        <footer>
+            <hr />
+            <span class="text-reset">Mark Bird</span>
+            <br />
+            <a href="https://github.com/mbird13/startup">GitHub</a>
+        </footer>
+  </div>;
 }
