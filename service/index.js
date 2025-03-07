87469270d9eafa1231ec66a5e8412247cff49596
Mark Bird <markrbird13@gmail.com>
2025-03-07 11:42:37 -0700

    added more dependencies and stubs for endpoints

--- service/index.js
+++ service/index.js
@@ -1,1 +1,21 @@
+import express from 'express';
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app = express();
+
+app.use(express.static('public'));
+
+const verifyAuth = (req, res, next) => {
+    // check if the user is authenticated
+    next();
+}
+
+app.post('/api/recipe', verifyAuth, (req, res) => {
+    //add a recipe to the user's favorites
+    res.sent("Recipe added to favorites");
+
+});
+
+
+app.listen(port);
