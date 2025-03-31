0bf0326419c497c6b4f52120437138d75491f49d
Mark Bird <markrbird13@gmail.com>
2025-03-31 15:53:41 -0600

    removed unused code

--- src/home/Notifier.js
+++ src/home/Notifier.js
@@ -12,25 +12,12 @@ class Notifier {
         let port = window.location.port;
         const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
         this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
-        // this.socket.onopen = (event) => {
-        //     this.recieveNotification(new Notification(username, 'logged in'));
-        //     this.sendNotification(new Notification(username, 'logged in'));
-        // };
-        // this.socket.onclose = (event) => {
-        //     this.recieveNotification(new Notification('you', 'disconnected'));
-        // };
         this.socket.onmessage = async (msg) => {
         try {
             const event = JSON.parse(await msg.data.text());
             this.receiveNotification(event);
         } catch {}
         };
-        // setInterval(() => {
-        //     const user = 'James';
-        //     const value = 'Drunken Noodles';
-        //     this.sendNotification('James', 'Drunken Noodles');
-
-        // }, 5000);
     }
 
     sendNotification(from, value) {
